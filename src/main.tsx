af869f8713e7e00d66d792b3b6aae90329756a00
Danil Mamulian <icemeat2004@gmail.com>
2023-11-21 14:13:21 +0300

    feature dns13 routing updateversion

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './app/Routing';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
