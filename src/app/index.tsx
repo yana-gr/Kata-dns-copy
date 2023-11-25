3bda2053e0d7260e8a63f2e5e5c02e40c9b47139
YanaGrinko <yana_grinko@mail.ru>
2023-11-26 01:27:05 +0300

    DNS-2 Create state manager

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,17 @@
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom';
 import { router } from './routing/Routing';
 import React from 'react';
+import store from '../shared/store/store';
+import { Provider } from 'react-redux';
 
 const App = () => {
   return (
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </React.StrictMode>
   );
-}
+};
 
-export default App
+export default App;
